93a9bea454635e9dc21a7b8cb58b1e8b0a1634ec
marwabekhouche <93099811+marwabekhouche@users.noreply.github.com>
2022-01-15 23:19:08 +0100

    Add files via upload

---
+++ de.js
@@ -0,0 +1,57 @@
+ react,{components}   ;
+
+
+function de(props) {
+    console.log(props);
+    console.log(props);
+      
+}
+
+
+export default de ;
+
+function random(props) {
+    console.log(props)
+    return(
+        <div className="app">
+            props.random({props.name.random})
+           
+        </div>
+               
+    )
+    
+}
+export default random;
+
+
+class fin extends components{
+    render(){
+    constructor(props)
+       super(props)
+       if (props.name="1"=random.props) {
+
+           <html><img src="1.png"></img></html>   
+       } 
+       if (props.name="2"=random.props) {
+
+        <html><img src="2.png"></img></html>
+       }
+       if (props.name="3"=random.props) {
+
+        <html><img src="3.png"></img></html>
+           
+       }
+       if (props.name="4"=random.props) {
+        <html><img src="4.png"></img></html>
+           
+       }
+       if (props.name="5"=random.props) {
+        <html><img src="5.png"></img></html>
+           
+       }
+       if (props.name="6"=random.props) {
+        <html><img src="6.png"></img></html>
+           
+       }
+    }
+}
